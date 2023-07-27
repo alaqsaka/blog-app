31ba539aba0e99fd0e0b9e90ae6b87791d5b7f23
alaqsaka <aqsaabidin15@gmail.com>
2023-07-27 16:12:05 +0700

    blog detail page

--- app/(blog)/blog/[blogId]/page.tsx
+++ app/(blog)/blog/[blogId]/page.tsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const BlogPage = () => {
-  return <div>BlogPage</div>;
+  return (
+    <div className="w-4/6 mx-auto">
+      <h1 className="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight dark:text-white lg:text-4xl ">
+        Earum textilis annus dolores sursum demoror suasoria
+      </h1>
+      <div className="mt-3 flex justify-center space-x-3 text-gray-500 ">
+        <p className="font-semibold text-gray-900">
+          <a>
+            <span className="" />
+            User 3944168
+          </a>
+        </p>
+      </div>
+
+      <article className="mx-auto max-w-screen-md mt-5">
+        <div className="prose mx-auto my-3 dark:prose-invert prose-a:text-blue-600">
+          Adimpleo avoco custodia. Cum consequatur ipsam. Vomer denuo trepide.
+          Expedita alius somnus. Damno cunctatio ad. Facere vilitas adeptio.
+          Terra sed ut. Curis concedo desparatus. Arcesso adeptio brevis.
+          Apparatus universe benevolentia. Desolo quisquam viscus. Vulnus
+          aliquid cuius. Voluptatem tergeo damno.
+        </div>
+      </article>
+    </div>
+  );
 };
 
 export default BlogPage;
