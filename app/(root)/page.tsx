dda6d8ddd333dabd54f76f5a4cb0e7dd0ee6322b
alaqsaka <aqsaabidin15@gmail.com>
2023-07-27 20:38:54 +0700

    fix fetch

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,18 @@
 import BlogCard from "@/components/ui/BlogCard";
-import getPosts from "../actions/getPosts";
+
+async function getData() {
+  const res = await fetch("https://gorest.co.in/public/v2/posts");
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
 
 interface Post {
   id: string;
@@ -9,14 +22,12 @@ interface Post {
 }
 
 export default async function Home() {
-  const posts = await getPosts();
-
-  console.log("posts data ", posts?.data);
+  const posts = await getData();
 
   return (
     <div className="w-4/6 mx-auto">
       <div className="mx-auto grid mt-10 max-w-2xl grid-cols-1 gap-x-8 gap-y-16  lg:mx-0 lg:max-w-none lg:grid-cols-3">
-        {posts?.data.map((post: Post) => (
+        {posts.map((post: Post) => (
           <BlogCard data={post} key={post.id} />
         ))}
       </div>
