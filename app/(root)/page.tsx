72598df2392160f5c234ea9ffc6e5037d46bfe5b
alaqsaka <aqsaabidin15@gmail.com>
2023-07-29 22:05:02 +0700

    add pagination for blogs

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,20 @@
 import BlogCard from "@/components/ui/BlogCard";
 import { Suspense } from "react";
 import Loading from "./loading";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-async function getData() {
-  const res = await fetch("https://gorest.co.in/public/v2/posts", {
-    headers: {
-      Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
-    },
-    cache: "no-store",
-    next: { revalidate: 0 },
-  });
+async function getData(page: number) {
+  const res = await fetch(
+    `https://gorest.co.in/public/v2/posts?page=${page}&per_page=15`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+      },
+      cache: "no-store",
+      next: { revalidate: 0 },
+    }
+  );
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
@@ -29,8 +34,13 @@ interface Post {
   body: string;
 }
 
-export default async function Home() {
-  const posts = await getData();
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  const page = parseInt(searchParams.page) || 1;
+  const posts = await getData(page);
 
   return (
     <div className="w-5/6 lg:w-4/6 mx-auto">
@@ -41,6 +51,24 @@ export default async function Home() {
           ))}
         </div>
       </Suspense>
+      <div className="text-center mt-10 flex-row flex items-center justify-center gap-4">
+        <Link
+          href={`/?page=${page - 1 !== 0 && page - 1 !== 1 ? page - 1 : "1"}`}
+        >
+          <Button
+            disabled={page < 0 || page == 1}
+            className="w-[100px]"
+            variant="outline"
+          >
+            Previous
+          </Button>
+        </Link>
+        <Link href={`?page=${page + 1}`}>
+          <Button className="w-[100px]" variant="outline">
+            Next
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
