635b56e0ecc5a0d4b9bdc2f9c1d6dbb0ef76a043
alaqsaka <aqsaabidin15@gmail.com>
2023-07-29 21:27:23 +0700

    create footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ToasterProvider } from "@/providers/toast-provider";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <ToasterProvider />
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
