5145c2e4c1a77edda8378a85078aa644a6da7dd5
alaqsaka <aqsaabidin15@gmail.com>
2023-07-29 22:08:42 +0700

    add pagination for users page

--- app/(user)/users/page.tsx
+++ app/(user)/users/page.tsx
@@ -2,10 +2,12 @@ import React, { Suspense } from "react";
 import { DataTable } from "./components/data-table";
 import { columns } from "./components/columns";
 import LoadingUsers from "./loading";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
-async function getUsersData() {
+async function getUsersData(page: number) {
   const res = await fetch(
-    `https://gorest.co.in/public/v2/users?page1&per_page=100`,
+    `https://gorest.co.in/public/v2/users?page=${page}&per_page=15`,
     {
       headers: {
         Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
@@ -34,16 +36,40 @@ interface User {
   status: string;
 }
 
-const UserPage = async () => {
-  const users = await getUsersData();
-
-  console.log("data", users);
+const UserPage = async ({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) => {
+  const page = parseInt(searchParams.page) || 1;
+  const users = await getUsersData(page);
 
   return (
     <div className="w-5/6 lg:w-4/6 mx-auto">
       <Suspense fallback={<LoadingUsers />}>
         <DataTable columns={columns} data={users} />
       </Suspense>
+
+      <div className="text-center mt-10 flex-row flex items-center justify-center gap-4">
+        <Link
+          href={`/users?page=${
+            page - 1 !== 0 && page - 1 !== 1 ? page - 1 : "1"
+          }`}
+        >
+          <Button
+            disabled={page < 0 || page == 1}
+            className="w-[100px]"
+            variant="outline"
+          >
+            Previous
+          </Button>
+        </Link>
+        <Link href={`/users?page=${page + 1}`}>
+          <Button className="w-[100px]" variant="outline">
+            Next
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
